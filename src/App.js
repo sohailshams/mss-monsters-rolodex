807b1f200573c2d4f52e1649c490da1bb0765bed
sohailshams <sohailshamas@yahoo.com>
2023-01-03 10:57:55 +0000

    Converted App component to functional component from class component.

--- src/App.js
+++ src/App.js
@@ -1,52 +1,42 @@
-import { Component } from "react"; 
+import {useState, useEffect} from 'react';
 import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
 
-class App extends Component {
-  constructor() {
-      super();
-      this.state = {
-        monsters: [],
-        searchField:''
-      };
-    }
 
-    componentDidMount() {
-      fetch("https://jsonplaceholder.typicode.com/users")
+const App = () => {
+  const [searchField, setSearchField] = useState('');
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
        .then(response => response.json())
-       .then(data => this.setState(() => {
-         return {
-           monsters: data
-         };
-       }));
-    }
+       .then(data => setMonsters(data)); 
+  }, [])
 
-    // Getting search input value and updating searchField variable value
-    onSearchChange = (event) => {
-      const searchField = event.target.value.toLocaleLowerCase();
-      this.setState(() => {
-        return {searchField};
-        });
-    }
-
-  render() {
-    const { monsters, searchField } = this.state;
-     const { onSearchChange } = this;
-    // Filtering monsters array based on seach input field value
-    const filteredMonsters = monsters.filter((monster) => {
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
       return monster.name.toLocaleLowerCase().includes(searchField);
-    });
+    }); 
 
-    return (
-      <div className="min-h-full items-center bg-gradient-to-r from-cyan-500 to-blue-500 text-gray-700">
-        <div className="max-w-[90%] mx-auto">
-          <h1 className="text-center py-16 font-bold text-[30px] font-['Neucha']">Monster Rolodex</h1>
-        <SearchBox onChangeHandler={onSearchChange} placeholder='Search Monsters' className='search-box' />
-       <CardList monsters={filteredMonsters} />
-      </div></div>
-    );
+    setFilteredMonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
+
+   // Getting search input value and updating searchField variable value
+   const onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    setSearchField(searchField);
   }
- 
+
+  return (
+    <div className="min-h-full items-center bg-gradient-to-r from-cyan-500 to-blue-500 text-gray-700">
+      <div className="max-w-[90%] mx-auto">
+        <h1 className="text-center py-16 font-bold text-[30px] font-['Neucha']">Monster Rolodex</h1>
+        <SearchBox onChangeHandler={onSearchChange} placeholder='Search Monsters' className='search-box' />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    </div>
+  )
 }
 
 export default App;
